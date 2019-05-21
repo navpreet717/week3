e9962d0e0acacab96c2bbf5276b37060a29074fe
748674 <748674@123-PC16.student.Tor.Loc>
2019-05-21 15:33:37 -0400

    finished sum of numbers

--- src/SumOfTwoNumbers.java
+++ src/SumOfTwoNumbers.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 
 public class SumOfTwoNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//get number from user
+		Scanner keyboard=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int number=keyboard.nextInt();
+		//add the numbers
+		int total= 0;
+		//for loop
+		for(int i=0;i<=number;i++){
+			total=total+i;
+		}
 		
-
+		//output
+		System.out.println("The total is:"+total);
+		
 	}
 
 }
